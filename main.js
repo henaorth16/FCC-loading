07bf97bcc3e1ced1d09eacef535334f1f337a8e4
henaorth16 <emyayehenok@gmail.com>
2025-01-10 22:52:18 +0300

    UPDATE: code efficiency

--- main.js
+++ main.js
@@ -15,27 +15,42 @@ function apply() {
 }
 
 function start(numberOfRect, delay, speed) {
-   main.innerHTML = ""
-   if(numberOfRect > 0 && delay >= 0 && speed >= 0){
-      let center = Math.ceil(numberOfRect / 2);
-      let target = center * 2
-      
-      for (let i = 0; i < numberOfRect; i++) {
-         const divElem = document.createElement("div")
-         divElem.style.animationDuration = `${speed}ms`
-         console.log(speed)
-         main.appendChild(divElem)
-      }
-      for (let i = 1; i < center; i++) {
-         const rects = document.querySelectorAll("#main > *")
+   main.innerHTML = "" // First,  clear if there is any garbage inn it
+   if(numberOfRect > 0 && delay >= 0 && speed >= 0){  //check the input is valid
+      //create divs or rectangles
+     for (let i = 0; i < numberOfRect; i++) {
+       const divElem = document.createElement("div");
+       divElem.style.animationDuration = `${speed}ms`;
 
-         let left = i
-         let right = target - (i+2)
-         console.log(rects);
+       main.appendChild(divElem);
+     }
+     /* ########### For loop (old way) ########## */
+     // let center = Math.ceil(numberOfRect / 2);
+     // let target = center * 2;
 
-         rects[left].style.animationDelay = `${i*delay}ms`
-         rects[right].style.animationDelay = `${i * delay}ms`
-      }
+     // for (let i = 1; i < center; i++) {
+     //    const rects = document.querySelectorAll("#main > *")
+
+     //    let left = i
+     //    let right = target - (i+2)
+     //    console.log(rects);
+
+     //    rects[left].style.animationDelay = `${i*delay}ms`
+     //    rects[right].style.animationDelay = `${i * delay}ms`
+     // }
+
+     /* ####### while loop (new way) ###### */
+     const rects = document.querySelectorAll("#main > div");
+     let left = 0;
+     let right = rects.length - 1;
+     while (left <= right) {
+       console.log(left, right);
+       rects[left].style.animationDelay = `${left * delay}ms`;
+       rects[right].style.animationDelay = `${left * delay}ms`;
+       left++;
+       right--;
+     }
+
    }else{
       main.innerHTML = `
       <h2>Invalid input</h2>
